d5361a56e82617a641546513528510ba173be0c5
deividsonsabino <deividsonsabino@outlook.com.br>
2022-04-06 10:55:04 -0300

    :sparkles: virtualização

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized"
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -14,19 +14,33 @@ interface SearchResultsProps {
 
 
 export function SearchResults({ results, totalPrice, onAddWichlist }: SearchResultsProps) {
+    const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+        return (
+            <div
+                key={key}
+                style={style}
+            >
+                <ProductItem
+                    key={key}
+                    product={results[index]}
+                    onAddWichlist={onAddWichlist}
+                />
+            </div>
+        )
+    }
 
     return (
         <div>
             <h2>{totalPrice}</h2>
-            {results.map(product => {
-                return (
-                    <ProductItem
-                        key={product.id}
-                        product={product}
-                        onAddWichlist={onAddWichlist}
-                    />
-                )
-            })}
+
+            <List
+                height={300}
+                rowHeight={30}
+                width={900}
+                overscanRowCount={5}
+                rowCount={results.length}
+                rowRenderer={rowRenderer}
+            />
         </div>
     )
 }
